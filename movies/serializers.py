269380440f2cfedb88c6efb41c3f90d459f9a96a
Gabriel Castro <gabrielcssg@gmail.com>
2025-01-13 23:39:52 -0300

    feat: movie serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,32 @@
-from rest_framework.serializers import ModelSerializer
+from django.db.models import Avg
+from rest_framework import serializers
 
 from movies.models import Movie
 
 
-class MovieSerializer(ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError(
+                "O ano do filme não pode ser menor que 1900.")
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError(
+                "O resumo não pode exceder 500 characters.")
+        return value
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
+        return None
